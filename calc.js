592f129231ca8214ca79d5f9cd304b55eba8432f
Max Botwin <maxmarbot@gmail.com>
2021-09-20 16:04:33 -0500

    Addition implementation working

--- calc.js
+++ calc.js
@@ -1,21 +1,42 @@
 const readline = require('readline');
 
 const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate?", function (input) {
 	tokens = input.split(' ');
-	
+
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
 
+	switch (mathSymbol) {
+		case '+':
+			console.log(num1 + num2);
+			break;
+
+		case '-':
+			console.log(num1 - num2);
+			break;
+
+		case '*':
+			console.log(num1 * num2);
+			break;
+
+		case '/':
+			console.log(num1 / num2);
+			break;
+
+		case 'sqrt':
+			console.log(Math.sqrt(num1));
+			break;
+	}
 	// This line closes the connection to the command line interface.
 	reader.close()
-
 });
+
